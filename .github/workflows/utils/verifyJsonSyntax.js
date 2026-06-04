d27d4cdb1a11308588f37f6ef708c901b5ca3580
Thalya Steffany <contato.thalyasteffany@gmail.com>
2026-06-04 10:43:05 -0300

    fix: general issues

--- .github/workflows/utils/verifyJsonSyntax.js
+++ .github/workflows/utils/verifyJsonSyntax.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-function verifyJsonSyntax(json) {
+export function verifyJsonSyntax(json) {
   try {
     const jsonString = typeof json !== "string" ? JSON.stringify(json) : json;
     JSON.parse(json);
@@ -11,3 +11,4 @@ function verifyJsonSyntax(json) {
     return false;
   }
 }
+
