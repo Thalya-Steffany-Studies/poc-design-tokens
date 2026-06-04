7cce6a297687489784393743b3d4928c4dc1bbb7
Thalya Steffany <contato.thalyasteffany@gmail.com>
2026-06-04 08:39:07 -0300

    feat: create functions to valid tokens format

---
+++ .github/workflows/utils/executeSyntaxValidation.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+
+function executeSytaxValidation() {
+    /**Get filename add as param in GitHub Actions*/
+  const gthActFileName = process.argv[2];
+
+  if (!gthActFileName) {
+    console.error("No files were specified in the workflow!");
+    process.exit(1);
+  }
+
+  const filePath = path.resolve(gthActFileName);
+
+  try {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+
+    const isAValidJson = verifyJsonSyntax(fileContent);
+
+    if (isAValidJson) {
+      console.log(`The file '${gthActFileName}' is a valid JSON.`);
+      process.exit(0);
+    } else {
+      console.error(`Syntax error foude at '${gthActFileName}':`);
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error(`Error to read '${gthActFileName}':`);
+    console.error(error);
+    process.exit(1);
+  }
+}
+
---
+++ .github/workflows/utils/verifyJsonSyntax.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+
+function verifyJsonSyntax(json) {
+  try {
+    const jsonString = typeof json !== "string" ? JSON.stringify(json) : json;
+    JSON.parse(json);
+    return true;
+  } catch (e) {
+    console.error({ e });
+    return false;
+  }
+}
